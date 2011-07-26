0f349e46d6fb36a13bf6920422eded0aff807599
Oscar Koeroo <okoeroo@gmail.com>
2011-07-26 19:22:48 +0200

    Big rewrite

--- tor-arm-replace-torrc.py
+++ tor-arm-replace-torrc.py
@@ -25,136 +25,133 @@ GROUP = "tor-arm"
 TOR_CONFIG_FILE = "/etc/tor/torrc"
 ARM_CONFIG_FILE = "/var/lib/tor-arm/torrc"
 
-if __name__ == "__main__":
-  # sanity check that we're on linux
-  if os.name != "posix":
-    print "This is a script specifically for configuring Debian Gnu/Linux"
-    sys.exit(1)
 
-  orig_uid = os.getuid()
-  orig_euid = os.geteuid()
-  orig_gid = os.getgid()
-  orig_egid = os.getegid()
-  # We must have USER and GROUP 
-  try:
-    dropped_id_pwn = pwd.getpwnam(USER)
-  except:
-    print "Our tor-arm user was not found - this is unsafe; exiting now!"
-    exit(1)
-  dropped_uid = dropped_id_pwn.pw_uid
-  dropped_euid = dropped_id_pwn.pw_uid
-  dropped_gid = grp.getgrnam(GROUP).gr_gid
-  dropped_egid = grp.getgrnam(GROUP).gr_gid
-  
-  # check that we're running effectively as root
-  if orig_euid != 0:
-    print "This script needs to be run as root"
-    sys.exit(1)
-  
-  # if we're actually root, we skip this group check
-  # root can get away with all of this
-  if orig_uid != 0:
-    # check that the user is in GROUP
-    try:
-      # checks if that's a group we're in
-      if not dropped_gid in os.getgroups():
-        print "Your user needs to be a member of the %s group for this to work" % GROUP
-        sys.exit(1)
-    except KeyError:
-      print "The %s group doesn't exist on this system" % GROUP
-      sys.exit(1)
 
-  # Drop to the unpriv'ed group, and really lose the rest of the groups
-  os.setgid(dropped_gid)
-  os.setegid(dropped_egid)
-  os.setresgid(dropped_gid, dropped_egid, dropped_gid)
-  os.setgroups([dropped_gid])
-   
-  # open a tempfile and chown it to 116
-  # Make a tempfile and write out the contents
-  try:
-    tf = tempfile.NamedTemporaryFile(delete=False) # This uses mkstemp internally
-    os.chown(tf.name, dropped_uid, orig_euid) # this allows our child process to write to tf.name (not only if their uid matches, not their gid) 
-  except:
-    print "We were unable to make a temporary file"
-    sys.exit(1)
+class tor_arm_replace_torrc(object):
+    def __init__(self):
+        if os.name != "posix":
+            print "This is a script specifically for configuring Debian Gnu/Linux and other Unix like systems"
+            sys.exit(1)
 
-  parent_pid = os.getpid()
-  fork_pid = os.fork()
-  # open the suspect config after we drop privs
-  # we assume the dropped privs are still enough to write to the tf
-  if (fork_pid == 0):
-    signal.signal(signal.SIGCHLD, signal.SIG_IGN)
-    # Drop privs forever in the child process
-    # I believe this drops os.setfsuid os.setfsgid stuff
-    # Clear all other supplemental groups for dropped_uid
-    os.setgroups([dropped_gid])
-    os.setresgid(dropped_gid, dropped_egid, dropped_gid)
-    os.setresuid(dropped_uid, dropped_euid, dropped_uid)
-    os.setgid(dropped_gid)
-    os.setegid(dropped_egid)
-    os.setuid(dropped_uid)
-    os.seteuid(dropped_euid)
-    
-    try:
-      af = open(ARM_CONFIG_FILE) # this is totally unpriv'ed
-      # ensure that the fd we opened has the properties we requrie
-      configStat = os.fstat(af.fileno()) # this happens on the unpriv'ed FD
-      if configStat.st_gid != dropped_gid:
-        print "Arm's configuration file (%s) must be owned by the group %s" % (ARM_CONFIG_FILE, GROUP)
-        sys.exit(1)
-      # if everything checks out, we're as safe as we're going to get
-      armConfig = af.read(1024 * 1024) # limited read but not too limited
-      af.close()
-      tf.file.write(armConfig)
-      tf.flush()
-    except:
-      print "Unable to open the arm config as unpriv'ed user"
-      sys.exit(1)
-    finally:
-      tf.close()
-      sys.exit(0)
-  else:
-    # If we're here, we're in the parent waiting for the child's death
-    # man, unix is really weird...
-    child_pid, status = os.waitpid(fork_pid, 0)
-  if status != 0:
-    print "The child seems to have failed; exiting!"
-    tf.close()
-    sys.exit(1)
+        # 1. Remove the environment, do not pass go otherwise
+        remove_environment()
 
-  # attempt to verify that the config is OK
-  if os.path.exists(tf.name):
-    # construct our SU string
-    SUSTRING = "su -c 'tor --verify-config -f " + str(tf.name) + "' " + USER
-    # We raise privs to drop them with 'su'
-    os.setuid(0)
-    os.seteuid(0)
-    os.setgid(0)
-    os.setegid(0)
-    # We drop privs here and exec tor to verify it as the dropped_uid 
-    print "Using Tor to verify that arm will not break Tor's config:"
-    success = os.system(SUSTRING)
-    if success != 0:
-      print "Tor says the new configuration file is invalid: %s (%s)" % (ARM_CONFIG_FILE, tf.name)
-      sys.exit(1)
+        # 2. Set the trusted user and group information
+        set_trusted_account_info(USER, GROUP)
 
-  # backup the previous tor config
-  if os.path.exists(TOR_CONFIG_FILE):
-    try:
-      backupFilename = "%s_backup_%i" % (TOR_CONFIG_FILE, int(time.time()))
-      shutil.copy(TOR_CONFIG_FILE, backupFilename)
-    except IOError, exc:
-      print "Unable to backup %s (%s)" % (TOR_CONFIG_FILE, exc)
-      sys.exit(1)
-  
-  # overwrites TOR_CONFIG_FILE with ARM_CONFIG_FILE as loaded into tf.name
-  try:
-    shutil.copy(tf.name, TOR_CONFIG_FILE)
-    print "Successfully reconfigured Tor"
-  except IOError, exc:
-    print "Unable to copy %s to %s (%s)" % (tf.name, TOR_CONFIG_FILE, exc)
-    sys.exit(1)
+        # 3. Set configuration file information
+        self.tor_config_file = TOR_CONFIG_FILE
+        self.arm_config_file = ARM_CONFIG_FILE
+
+        # 4. Am I root? - We need to be root, or effective root. Without it continuation is futile
+        if not self.got_sufficient_privileges():
+            print "Sorry, insufficient privileges. Continuation is futile"
+            sys.exit(1)
+
+        # 4b. Continue with the change_configuration()
+        # init is done
+
+    ### Run in init
+    def remove_environment(self):
+        for i in os.environ:
+            print i
+            os.unsetenv(i)
+
+    ### Run in init
+    def set_trusted_account_info(self, user="tor-arm", group="tor-arm"):
+        self.trusted_user  = user
+        self.trusted_group = group
+        try:
+            self.trusted_uid = pwd.getpwnam(USER).pw_uid
+        except:
+            print "Our tor-arm user \"%s\" was not found - this is unsafe; exiting now!" % user
+            sys.exit(1)
+
+        try:
+            self.trusted_gid = grp.getgrnam(GROUP).gr_gid
+        except:
+            print "Our tor-arm group \"%s\" was not found - this is unsafe; exiting now!" % group
+            sys.exit(1)
+
+    ### Run in init
+    def got_sufficient_privileges(self, group="tor-arm"):
+        # Am I the big man on the system?
+        if os.getuid() == 0:
+            return True
+
+        # Can I effectively play as the big cheeze?
+        if os.geteuid() == 0:
+            print "This script requires (effective) root privileges. Can't continue without it"
+            sys.exit(1)
+
+        # Ok, effective root, but am I privileged?
+        try:
+            # checks if that's a group we're in
+            if self.trusted_group in os.getgroups():
+                return True
+            else:
+                print "Your user needs to be a member of the %s group for this to work" % self.trusted_group
+                return False
+        except:
+            # Made pedantic
+            print "The %s group doesn't exist on this system" % self.trusted_group
+            return False
+
+        return False
+
+    def drop_privileges(self):
+        # Drop to the unpriv'ed group, and really lose the rest of the groups
+        # - Must set one group to overcome old BSD and current OSX bugs
+        try:
+            os.setgroups([self.trusted_gid])
+            os.setgid(self.trusted_gid)
+            os.setegid(self.trusted_gid)
+
+            os.setuid(self.trusted_uid)
+            os.seteuid(self.trusted_uid)
+        except:
+            print "Error: couldn't drop privileges. Do I check myself to be sufficiently privileged to get here?"
+            sys.exit(1)
+
+    def reraise_privs(self):
+        os.setuid(0)
+        os.seteuid(0)
+        os.setgid(0)
+        os.setegid(0)
+        # Secondary GIDs don't have a saved equivalent
+
+    def backup_configuration_file(self):
+        # backup the previous tor config
+        if os.path.exists(TOR_CONFIG_FILE):
+            try:
+                # TODO: Safety check
+                backupFilename = "%s_backup_%i" % (TOR_CONFIG_FILE, int(time.time()))
+                shutil.copy(TOR_CONFIG_FILE, backupFilename)
+            except IOError, exc:
+                print "Unable to backup %s (%s)" % (TOR_CONFIG_FILE, exc)
+                sys.exit(1)
+
+    def is_configuration_file_correct(self, configfile):
+        TOR_VERIFY_SHELL = 'tor --verify-config -f ' + configfile
+
+        print "Using Tor to verify that arm will not break Tor's configuration"
+        rc = os.system(TOR_VERIFY_SHELL)
+        if rc == 0:
+            return True
+        else:
+            print "Tor says the new configuration file is invalid: \"%s\" (%s)" % configfile
+            return False
+
+    def act_like_a_parent(self, child_pid):
+        # Wait until our child got back - It should have succefully written the configuration file
+        child_pid, status = os.waitpid(child_pid, 0)
+
+        # Done?
+        sys.exit(0)
+
+        # As parents we can cheat and reraise privileges - full root now
+        #self.reraise_privs()
+
 
   # unlink our temp file
   try:
@@ -163,4 +160,70 @@ if __name__ == "__main__":
     print "Unable to close temp file %s" % tf.name
     sys.exit(1)
 
-  sys.exit(0)
+    def act_like_a_child(self):
+        signal.signal(signal.SIGCHLD, signal.SIG_IGN)
+
+        # Make a tempfile and write out the contents
+        try:
+            # TODO: Check path before HERE!!!
+            tf = tempfile.NamedTemporaryFile(delete=False) # This uses mkstemp internally
+        except:
+            print "We were unable to make a temporary file"
+            sys.exit(1)
+
+
+            try:
+                af = open(ARM_CONFIG_FILE) # this is totally unpriv'ed
+                # ensure that the fd we opened has the properties we requrie
+                configStat = os.fstat(af.fileno()) # this happens on the unpriv'ed FD
+                if configStat.st_gid != dropped_gid:
+                    print "Arm's configuration file (%s) must be owned by the group %s" % (ARM_CONFIG_FILE, GROUP)
+                sys.exit(1)
+        # if everything checks out, we're as safe as we're going to get
+                    armConfig = af.read(1024 * 1024) # limited read but not too limited
+                    af.close()
+                    tf.file.write(armConfig)
+                tf.flush()
+                    except:
+                    print "Unable to open the arm config as unpriv'ed user"
+                sys.exit(1)
+                    finally:
+                    tf.close()
+                sys.exit(0)
+
+        # Check the configuration file for correctness
+        if self.is_configuration_file_correct('FFOOOOFOFOFOF'):
+            print "Configuration file is correct"
+        else:
+            print "Configuration file is unusable, bailing out"
+            sys.exit(1)
+
+    def spoon(self):
+        fork_pid = os.fork()
+        if (fork_pid == 0):
+            # Start to play ball
+            self.act_like_a_child()
+        else:
+            # Start parenting
+            self.act_like_a_parent(fork_pid)
+
+    def change_configuration(self):
+        # I'm (effective) root here
+        # 5. Backup current configuration file
+        self.backup_configuration_file()
+
+        # 6. Lower privileges to a more humble level - less privileges, more trust
+        self.drop_privileges()
+
+        
+
+        # 7. Fork! - Parent will continue to wait until the child is done.
+        #            The child will exchange the configuration file
+        self.spoon()
+
+
+if __name__ == "__main__":
+    tor_arm = tor_arm_replace_torrc()
+    tor_arm.change_configuration()
+
+    sys.exit(0)
